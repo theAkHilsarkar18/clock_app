71ec7b539e659543e81595263f80d5be4c300958
akhil <akhilsodvadiya7@gmail.com>
2023-09-19 14:19:09 +0530

    19-09-2023

--- lib/ui/demoscrenn.dart
+++ lib/ui/demoscrenn.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class DemoScreen extends StatefulWidget {
@@ -6,14 +9,25 @@ class DemoScreen extends StatefulWidget {
   @override
   State<DemoScreen> createState() => _DemoScreenState();
 }
-
+DateTime dateTime = DateTime.now();
 class _DemoScreenState extends State<DemoScreen> {
+  @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if(dateTime != DateTime.now())
+          {
+            dateTime = DateTime.now();
+          }
+      });
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          leading: const Icon(Icons.menu_open_rounded),
           title: const Text(
             "Clock App",
           ),
@@ -22,11 +36,16 @@ class _DemoScreenState extends State<DemoScreen> {
           child: Container(
             height: 300,
             width: 300,
-            decoration: const BoxDecoration(
-              color: Colors.green,
+            decoration:  BoxDecoration(
+              color: Colors.deepPurpleAccent,
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.deepPurpleAccent,width: 2)
             ),
-            child: CustomPaint(
-              painter: ClockPainter(),
+            child: Transform.rotate(
+              angle: -pi/2,
+              child: CustomPaint(
+                painter: ClockPainter(),
+              ),
             ),
           ),
         ),
@@ -40,18 +59,46 @@ class ClockPainter extends CustomPainter
   @override
   void paint(Canvas canvas, Size size) {
 
-    double sizeX = size.height/2;
-    double sizeY = size.width/2;
-    Offset center =  Offset(sizeX,sizeY);
-    canvas.drawCircle(center, 10, Paint()..color = Colors.white);
+    var centerX = size.width/2;
+    var centerY = size.height/2;
+    Offset center = Offset(centerX, centerY);
+
+
+    // main circle
+    Paint mainCircle = Paint()..color = Colors.white;
+    canvas.drawCircle(center, 140, mainCircle);
+
+    // hour line
+    var hourX = centerX + size.width * 0.20 * cos((dateTime.hour * 0.5) * pi / 180);
+    var hourY = centerY + size.height * 0.20 * sin((dateTime.hour * 0.5) * pi / 180);
+    Paint hourLine = Paint()..color = Colors.blueAccent..strokeWidth = 15..strokeCap = StrokeCap.round;
+    canvas.drawLine(center, Offset(hourX, hourY), hourLine);
+
+    // minute line
+    var minuteX = centerX + size.width * 0.30 * cos((dateTime.minute * 6) * pi / 180 );
+    var minuteY = centerX + size.height * 0.30 * sin((dateTime.minute * 6) * pi / 180 );
+
+    Paint minuteLine = Paint()..color = Colors.deepPurple..strokeWidth = 7..strokeCap = StrokeCap.round;
+    canvas.drawLine(center, Offset(minuteX, minuteY), minuteLine);
+
+    // second line
+    var secondX = centerX + size.width * 0.38 * cos((dateTime.second * 6) * pi / 180);
+    var secondY = centerX + size.width * 0.38 * sin((dateTime.second * 6) * pi / 180);
+
+    Paint secondLine = Paint()..color = Colors.deepPurpleAccent..strokeWidth = 5..strokeCap = StrokeCap.round;
+    canvas.drawLine(center, Offset(secondX,secondY), secondLine);
+
+    // center dot circle
+    Paint centerDotCircle = Paint()..color = Colors.purpleAccent;
+    canvas.drawCircle(center, 15, centerDotCircle);
 
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    throw UnimplementedError();
+    return true;
   }
 
 }
+
 
